2a25455079d8ca586ea52ef4617aab899fa3b5bb
arya rai <178193084+awrya-cmd@users.noreply.github.com>
2026-01-10 21:54:19 +0530

    Time: 1 ms (76.25%), Space: 15 MB (19.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        vector<int> indices(2,-1);
-        int i, j, n;
+        unordered_map<int, int> mp; // value -> index
+        int n, i, req;
         n = nums.size();
-        for (i=0; i<n; i++)
+        for (i = 0; i < n; i++)
         {
-            for (j=i+1; j<n; j++)
-            {
-                if (nums[i]+nums[j]==target)
-                {
-                    indices[0] = i;
-                    indices[1] = j;
-                }
-            }
+            req = target - nums[i];
+            //if not in map, find return iterator to the after the  last element in the map ie end.
+            if (mp.find(req) != mp.end())
+                return {mp[req], i};
+            mp[nums[i]] = i;
         }
-        return indices;
+        return {-1,-1};
     }
 };
